30bc2f4f32af1a4e017ef5b6a09dc623017da526
Mateusz <mateusz.snamina@gmail.com>
2020-04-26 23:29:46 +0200

    utesting refactor

--- boson_algebra/src/transform_simplify_product.cpp
+++ boson_algebra/src/transform_simplify_product.cpp
@@ -20,22 +20,49 @@ ExpressionHandlerOptional transform_simplify_product(const ExpressionHandler& ex
     if (subproduct_hdl_iter == range_end) {
         return std::nullopt;
     }
-    const auto& subproduct_expr_hdl = *subproduct_hdl_iter;
-    ConstExpressionHandlerRandomAccessRange range1{range_begin, subproduct_hdl_iter};
-    ConstExpressionHandlerRandomAccessRange range2{subproduct_expr_hdl.target().crange()};
-    ConstExpressionHandlerRandomAccessRange range3{subproduct_hdl_iter + 1, range_end};
     ExpressionHandlerVector v;
-    for (const auto& subexpression_hdl : range1) {
-        v.emplace_back(std::move(subexpression_hdl.clone()));
-    }
-    for (const auto& subexpression_hdl : range2) {
-        v.emplace_back(std::move(subexpression_hdl.clone()));
-    }
-    for (const auto& subexpression_hdl : range3) {
-        v.emplace_back(std::move(subexpression_hdl.clone()));
+    for (const auto& subexpression_hdl : expression_hdl.target().crange()) {
+        if (subexpression_hdl.is_of_type<ProductExpression>()) {
+            for (const auto& subsubexpression_hdl : subexpression_hdl.target().crange()) {
+                v.emplace_back(std::move(subsubexpression_hdl.clone()));
+            }
+        } else {
+            v.emplace_back(std::move(subexpression_hdl.clone()));
+        }
     }
     ExpressionHandler product = ProductExpression::make(std::move(v));
     return std::move(product);
 }
+
+// ExpressionHandlerOptional transform_simplify_product_one(const ExpressionHandler& expression_hdl) {
+//     if (!expression_hdl.is_of_type<ProductExpression>()) {
+//         return std::nullopt;
+//     }
+//     const auto& range = expression_hdl.target().crange();
+//     const auto& range_begin = boost::begin(range);
+//     const auto& range_end = boost::end(range);
+//     const auto subproduct_hdl_iter = boost::find_if(range, [](const ExpressionHandler& expression_hdl) {
+//         return expression_hdl.is_of_type<ProductExpression>();
+//     });
+//     if (subproduct_hdl_iter == range_end) {
+//         return std::nullopt;
+//     }
+//     const auto& subproduct_expr_hdl = *subproduct_hdl_iter;
+//     ConstExpressionHandlerRandomAccessRange range1{range_begin, subproduct_hdl_iter};
+//     ConstExpressionHandlerRandomAccessRange range2{subproduct_expr_hdl.target().crange()};
+//     ConstExpressionHandlerRandomAccessRange range3{subproduct_hdl_iter + 1, range_end};
+//     ExpressionHandlerVector v;
+//     for (const auto& subexpression_hdl : range1) {
+//         v.emplace_back(std::move(subexpression_hdl.clone()));
+//     }
+//     for (const auto& subexpression_hdl : range2) {
+//         v.emplace_back(std::move(subexpression_hdl.clone()));
+//     }
+//     for (const auto& subexpression_hdl : range3) {
+//         v.emplace_back(std::move(subexpression_hdl.clone()));
+//     }
+//     ExpressionHandler product = ProductExpression::make(std::move(v));
+//     return std::move(product);
+// }
 
 }  // namespace boson_algebra
---
+++ boson_algebra_tests/include/boson_algebra_tests/abcd.hpp
@@ -0,0 +1,83 @@
+#ifndef BOSON_ALGEBRA_TEST_ABCD_HPP
+#define BOSON_ALGEBRA_TEST_ABCD_HPP
+
+#include <boson_algebra/expression_all.hpp>
+
+const std::shared_ptr<boson_algebra::Boson> a = std::make_shared<boson_algebra::CharBoson>('a');
+const std::shared_ptr<boson_algebra::Boson> b = std::make_shared<boson_algebra::CharBoson>('b');
+const std::shared_ptr<boson_algebra::Boson> c = std::make_shared<boson_algebra::CharBoson>('c');
+const std::shared_ptr<boson_algebra::Boson> d = std::make_shared<boson_algebra::CharBoson>('d');
+
+inline boson_algebra::ExpressionHandler cr_a() {
+    return boson_algebra::BosonCreationOperator::make(a);
+}
+
+inline boson_algebra::ExpressionHandler an_a() {
+    return boson_algebra::BosonAnihilationOperator::make(a);
+}
+
+inline boson_algebra::ExpressionHandler n_a() {
+    return boson_algebra::BosonNumberOperator::make(a);
+}
+
+inline boson_algebra::ExpressionHandler cr_b() {
+    return boson_algebra::BosonCreationOperator::make(b);
+}
+
+inline boson_algebra::ExpressionHandler an_b() {
+    return boson_algebra::BosonAnihilationOperator::make(b);
+}
+
+inline boson_algebra::ExpressionHandler n_b() {
+    return boson_algebra::BosonNumberOperator::make(b);
+}
+
+inline boson_algebra::ExpressionHandler cr_c() {
+    return boson_algebra::BosonCreationOperator::make(c);
+}
+
+inline boson_algebra::ExpressionHandler an_c() {
+    return boson_algebra::BosonAnihilationOperator::make(c);
+}
+
+inline boson_algebra::ExpressionHandler n_c() {
+    return boson_algebra::BosonNumberOperator::make(c);
+}
+
+inline boson_algebra::ExpressionHandler cr_d() {
+    return boson_algebra::BosonCreationOperator::make(d);
+}
+
+inline boson_algebra::ExpressionHandler an_d() {
+    return boson_algebra::BosonAnihilationOperator::make(d);
+}
+
+inline boson_algebra::ExpressionHandler n_d() {
+    return boson_algebra::BosonNumberOperator::make(d);
+}
+
+inline boson_algebra::ExpressionHandler make_expression_1() {
+    boson_algebra::ExpressionHandler double_cr_c = boson_algebra::IntegerFactoredExpression::make(2, cr_c());
+    boson_algebra::ExpressionHandler expr01 = boson_algebra::ProductExpression::make(cr_a(), an_b(), std::move(double_cr_c));
+    boson_algebra::ExpressionHandler expr02 = boson_algebra::SumExpression::make(cr_c(), cr_c());
+    boson_algebra::ExpressionHandler expr03 = boson_algebra::ProductExpression::make(an_d(), n_d());
+    boson_algebra::ExpressionHandler expr040 = boson_algebra::SumExpression::make(an_a(), n_b());
+    boson_algebra::ExpressionHandler expr041 = boson_algebra::SumExpression::make(an_c(), n_d());
+    boson_algebra::ExpressionHandler expr04 = boson_algebra::SumExpression::make(std::move(expr040), std::move(expr041));
+    boson_algebra::ExpressionHandler expr0 = boson_algebra::ProductExpression::make(std::move(expr01), std::move(expr02), std::move(expr03), std::move(expr04));
+    return expr0;
+}
+
+inline boson_algebra::ExpressionHandler make_expression_2() {
+    boson_algebra::ExpressionHandler double_cr_c = boson_algebra::IntegerFactoredExpression::make(2, cr_c());
+    boson_algebra::ExpressionHandler expr01 = boson_algebra::ProductExpression::make(cr_a(), an_b(), std::move(double_cr_c));
+    boson_algebra::ExpressionHandler expr02 = boson_algebra::SumExpression::make(cr_c(), cr_c());
+    boson_algebra::ExpressionHandler expr03 = boson_algebra::ProductExpression::make(an_d(), n_d());
+    boson_algebra::ExpressionHandler expr040 = boson_algebra::SumExpression::make(an_a(), n_b());
+    boson_algebra::ExpressionHandler expr041 = boson_algebra::SumExpression::make(an_c(), n_d());
+    boson_algebra::ExpressionHandler expr04 = boson_algebra::SumExpression::make(std::move(expr040), std::move(expr041));
+    boson_algebra::ExpressionHandler expr0 = boson_algebra::SumExpression::make(std::move(expr01), std::move(expr02), std::move(expr03), std::move(expr04));
+    return expr0;
+}
+
+#endif
