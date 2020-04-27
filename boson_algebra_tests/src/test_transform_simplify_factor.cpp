c1dea360a0302b6a72a5d837e78f9e30337890d0
Mateusz <mateusz.snamina@gmail.com>
2020-04-28 00:50:19 +0200

    finish tests for simplify factored

--- boson_algebra_tests/src/test_transform_simplify_factor.cpp
+++ boson_algebra_tests/src/test_transform_simplify_factor.cpp
@@ -1,5 +1,5 @@
 // SELF:
-#include<boson_algebra_tests/example_expressions.hpp>//TODO removed
+#include <boson_algebra_tests/example_expressions.hpp>  //TODO removed
 // TESTED LIBRARY:
 #include <boson_algebra/expression_all.hpp>
 #include <boson_algebra/expression_pragma.hpp>
@@ -33,4 +33,23 @@ TEST(TransformSimplifyFactor, OnProductOfBosonPrimitiveOperators) {
     ASSERT_EQ(expression.str(), "❪♯a◦♭b❫");
     const auto transformed_expression_optional = transform_simplify_factor(expression);
     ASSERT_FALSE(transformed_expression_optional);
+}
+
+TEST(TransformSimplifyFactor, OnFactorExpression) {
+    const auto expression = (4 * 'b'_an);
+    //std::cout << expression.str() << std::endl;
+    ASSERT_EQ(expression.str(), "❪4♭b❫");
+    const auto transformed_expression_optional = transform_simplify_factor(expression);
+    ASSERT_FALSE(transformed_expression_optional);
+}
+
+TEST(TransformSimplifyFactor, OnNestedFactorExpression) {
+    const auto expression = (4 * (-6 * 'b'_an));
+    //std::cout << expression.str() << std::endl;
+    ASSERT_EQ(expression.str(), "❪4❪-6♭b❫❫");
+    const auto transformed_expression_optional = transform_simplify_factor(expression);
+    ASSERT_TRUE(transformed_expression_optional);
+    const auto& transformed_expression = *transformed_expression_optional;
+    //std::cout << transformed_expression.str() << std::endl;
+    ASSERT_EQ(transformed_expression.str(), "❪-24♭b❫");
 }
