d03cd95af6aa9a6c96f24ef11e049630c5234437
Mateusz <mateusz.snamina@gmail.com>
2020-04-27 18:34:26 +0200

    start boson primitive operators testing

---
+++ boson_algebra_tests/src/test_boson_primitive_operators.cpp
@@ -0,0 +1,36 @@
+// SELF:
+#include <boson_algebra_tests/abcd.hpp>
+// TESTED LIBRARY:
+#include <boson_algebra/expression_all.hpp>
+#include <boson_algebra/transform_simplify_product.hpp>
+// GTEST:
+#include <gtest/gtest.h>
+// BOOST
+#include <boost/range/algorithm/count.hpp>
+
+namespace ba = boson_algebra;
+
+TEST(BosonPrimitiveOperator, BosonCreationOperator) {
+    const auto expression = cr_d();
+    ASSERT_EQ(expression.str(), "♯d");
+    ASSERT_EQ(expression.is_of_type<ba::BosonCreationOperator>(), true);
+    ASSERT_EQ(expression.is_of_type<ba::BosonAnihilationOperator>(), false);
+    ASSERT_EQ(expression.is_of_type<ba::BosonNumberOperator>(), false);
+    ASSERT_EQ(expression.target().n_subexpressions(), 0);     // TODO  remove target when wpoosible
+    ASSERT_EQ(boost::size(expression.target().range()), 0);   // TODO  remove target when wpoosible
+    ASSERT_EQ(boost::size(expression.target().crange()), 0);  // TODO  remove target when wpoosible
+
+    const auto expression1 = cr_d();
+    ASSERT_TRUE(expression.equals(expression1));
+    ASSERT_FALSE(std::addressof(cr_d().target()) == std::addressof(expression1.target()));
+
+    const auto expression2 = cr_a();
+    ASSERT_FALSE(expression.equals(expression2));
+    ASSERT_FALSE(std::addressof(cr_d().target()) == std::addressof(expression2.target()));
+
+    const auto expression3 = an_a();
+    ASSERT_FALSE(expression.equals(expression3));
+    ASSERT_FALSE(std::addressof(cr_d().target()) == std::addressof(expression3.target()));
+}
+
+//BosonAnihilationOperator
